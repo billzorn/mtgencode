40f5225d4401d3909e5aeee64883ee22ab2e9e2c
reimannsum <burkereimann@gmail.com>
2015-11-04 14:42:51 -0500

    changed mono-hybrid mana names to work with existing format

--- scripts/crons/process_output.sh
+++ scripts/crons/process_output.sh
@@ -4,19 +4,21 @@
 ## */30 * * * * ~/mtgencode/scripts/crons/process_output.sh 
 
 cd ~/mtgencode/cards
-pwd
+#pwd
 for folder in $(ls); do
 #	echo $folder
 	for sub in $( ls $folder );do 
 #		echo $folder/$sub
 		if [ ! -d $folder/$sub ]; then
+			echo $folder/$sub is not a directory.
 			continue
 		fi
-		if [ ! -e ./$folder/$sub/summary.txt ]; then
-			../scripts/summarize.py -a -v $folder/$sub/output.txt > summary.txt
+		if [ ! -e $folder/$sub/summary.txt ]; then
+			echo 'writing summary for ' $folder/$sub/output.txt
+			../scripts/summarize.py -a -v $folder/$sub/output.txt > $folder/$sub/summary.txt
 		fi
-		if [ ! -e  ./$folder/$sub/cards.txt ]; then
-			../decode.py -v -f $folder/$sub/output.txt $folder/$sub/cards.txt
+		if [ ! -e  $folder/$sub/cards.txt ]; then
+			../decode.py -v -f $folder/$sub/output.txt > $folder/$sub/cards.txt
 		fi
 	done
 done
