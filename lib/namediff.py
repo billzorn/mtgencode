18e1a66f885de79ca2a873893e68b9d7e570fb18
Bill Zorn <bill.zorn@gmail.com>
2015-11-08 19:57:47 -0800

    added parallel versions to cbow and namediff analyses, not used yet though

--- lib/namediff.py
+++ lib/namediff.py
@@ -1,10 +1,53 @@
+# This module is misleadingly named, as it has other utilities as well
+# that are generally necessary when trying to postprocess output by
+# comparing it against existing cards.
+
 import difflib
 import os
+import multiprocessing
+
 import jdecode
 import cardlib
 
 libdir = os.path.dirname(os.path.realpath(__file__))
 datadir = os.path.realpath(os.path.join(libdir, '../data'))
+
+# multithreading control parameters
+cores = multiprocessing.cpu_count()
+
+# split a list into n pieces; return a list of these lists
+# has slightly interesting behavior, in that if n is large, it can
+# run out of elements early and return less than n lists
+def list_split(l, n):
+    if n <= 0:
+        return l
+    split_size = len(l) / n
+    if len(l) % n > 0:
+        split_size += 1
+    return [l[i:i+split_size] for i in range(0, len(l), split_size)]
+
+# flatten a list of lists into a single list of all their contents, in order
+def list_flatten(l):
+    return [item for sublist in l for item in sublist]
+
+
+# isolated logic for multiprocessing
+def f_nearest(name, matchers, n):
+    for m in matchers:
+        m.set_seq1(name)
+    ratios = [(m.ratio(), m.b) for m in matchers]
+    ratios.sort(reverse = True)
+
+    if ratios[0][0] >= 1:
+        return ratios[:1]
+    else:
+        return ratios[:n]
+
+def f_nearest_per_thread(workitem):
+    (worknames, names, n) = workitem
+    # each thread (well, process) needs to generate its own matchers
+    matchers = [difflib.SequenceMatcher(b=name, autojunk=False) for name in names]
+    return map(lambda name: f_nearest(name, matchers, n), worknames)
 
 class Namediff:
     def __init__(self, verbose = True,
@@ -43,15 +86,11 @@ class Namediff:
         print '... Done.'
     
     def nearest(self, name, n=3):
-        for m in self.matchers:
-            m.set_seq1(name)
-        ratios = [(m.ratio(), m.b) for m in self.matchers]
-        ratios.sort(reverse = True)
+        return f_nearest(name, self.matchers, n)
 
-        if ratios[0][0] >= 1:
-            return ratios[:1]
-        else:
-            return ratios[:n]
-        
-            
-
+    def nearest_par(self, names, n=3, threads=cores):
+        workpool = multiprocessing.Pool(threads)
+        proto_worklist = list_split(names, threads)
+        worklist = map(lambda x: (x, self.names, n), proto_worklist)
+        donelist = workpool.map(f_nearest_per_thread, worklist)
+        return list_flatten(donelist)
